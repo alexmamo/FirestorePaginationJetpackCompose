83c3c74e13196c659893e497cb6c8c593f2855e5
alexm <alexmamo@gmail.com>
2022-01-23 15:38:02 +0200

    Minor fixes and improvements.

--- app/src/main/java/ro/alexmamo/firestorepaginationjetpackcompose/domain/model/Product.kt
+++ app/src/main/java/ro/alexmamo/firestorepaginationjetpackcompose/domain/model/Product.kt
@@ -1,7 +1,7 @@
 package ro.alexmamo.firestorepaginationjetpackcompose.domain.model
 
 data class Product (
-    var key: String? = null,
+    var id: String? = null,
     var name: String? = null,
     var price: Double? = null
 )
